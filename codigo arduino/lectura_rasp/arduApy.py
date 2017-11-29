11f137ea1ec7c5156ec9e4b8f26a8f3a863788a5
AntonioBarral <abarrg02@estudiantes.unileon.es>
2017-11-29 11:32:06 +0000

    Modificado script python para que añada registro a la tabla login. Da error de permisos a la hora de llamar a arduino

--- codigo arduino/lectura_rasp/arduApy.py
+++ codigo arduino/lectura_rasp/arduApy.py
@@ -1,7 +1,29 @@
 import serial
+import pymysql
+
 arduino = serial.Serial('/dev/ttyACM0', 9600)
+
 while True:
-	rawString = arduino.readline()
-	print(rawString)
+    user_ID = arduino.readline()
+    print(user_ID)
+    insert_record_value(user_ID)
 	
 arduino.close()
+
+
+def insert_record_vaue(user_ID):
+    connection = pymysql.connect("localhost", "phpmyadmin", "culodeadriana", "MagicMirror")
+    
+    cursor = connection.cursor()
+    sql_sentence = """INSERT INTO Login (ID) VALUES (user_ID)"""
+    
+    try:
+        cursor.execute(sql_sentence)
+        connection.commit()
+    except Exception as e:
+        print("Error al insertar valor en tabla Login \n")
+        print(e)
+        connection.rollback()
+        
+    connection.close()
+    return;
