92c59cd813342ca0b6e7078d3b04975f86a3688e
palvaf02 <palvaf02@estudiantes.unileon.es>
2018-01-05 22:01:03 +0100

    Revert "Merge branch 'development' of https://github.com/pabloaaf/Magic into development"
    This reverts commit 7434e42720964c7addce938fb11ed64d22a82ca9, reversing
    changes made to f5266fbbb3dec98005cbe02b1b563bbf961e6940.

--- MagicMirror.sql
+++ MagicMirror.sql
@@ -2,17 +2,10 @@
 -- version 4.6.6deb4
 -- https://www.phpmyadmin.net/
 --
-<<<<<<< HEAD
 -- Host: localhost:3306
 -- Generation Time: Jan 05, 2018 at 08:49 PM
 -- Server version: 10.1.23-MariaDB-9+deb9u1
 -- PHP Version: 7.0.19-1
-=======
--- Servidor: localhost:3306
--- Tiempo de generación: 10-12-2017 a las 18:38:44
--- Versión del servidor: 10.1.23-MariaDB-9+deb9u1
--- Versión de PHP: 7.0.19-1
->>>>>>> 63ec90df36b1bbf534b8d00d005a3fb5993fbe2f
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -24,20 +17,14 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
-<<<<<<< HEAD
 -- Database: `MagicMirror`
 --
 CREATE DATABASE IF NOT EXISTS `MagicMirror` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `MagicMirror`;
-=======
--- Base de datos: `MagicMirror`
---
->>>>>>> 63ec90df36b1bbf534b8d00d005a3fb5993fbe2f
 
 -- --------------------------------------------------------
 
 --
-<<<<<<< HEAD
 -- Table structure for table `Personalizaciones`
 --
 
@@ -107,81 +94,6 @@ ALTER TABLE `Usuario`
 --
 ALTER TABLE `Usuario`
   MODIFY `ID_Usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-=======
--- Estructura de tabla para la tabla `Login`
---
-
-CREATE TABLE `Login` (
-  `ID` int(11) NOT NULL,
-  `Hora_entrada` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `Login`
---
-
-INSERT INTO `Login` (`ID`, `Hora_entrada`) VALUES
-(5, '2017-12-10 00:00:01');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `Usuarios`
---
-
-CREATE TABLE `Usuarios` (
-  `ID_usuario` int(11) NOT NULL,
-  `Nombre` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `Usuarios`
---
-
-INSERT INTO `Usuarios` (`ID_usuario`, `Nombre`) VALUES
-(4, 'Adriana Alonso Yogures'),
-(2, 'Antonio Barral Cago (el P2)'),
-(5, 'Er root'),
-(3, 'Monica Alonso la Mateos'),
-(1, 'Pablo Alvarez (Veronica <3)');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `Login`
---
-ALTER TABLE `Login`
-  ADD PRIMARY KEY (`ID`,`Hora_entrada`);
-
---
--- Indices de la tabla `Usuarios`
---
-ALTER TABLE `Usuarios`
-  ADD PRIMARY KEY (`ID_usuario`),
-  ADD UNIQUE KEY `Nombre` (`Nombre`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `Usuarios`
---
-ALTER TABLE `Usuarios`
-  MODIFY `ID_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `Login`
---
-ALTER TABLE `Login`
-  ADD CONSTRAINT `Usuarios_Login` FOREIGN KEY (`ID`) REFERENCES `Usuarios` (`ID_usuario`);
-
->>>>>>> 63ec90df36b1bbf534b8d00d005a3fb5993fbe2f
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
